5acfb6ae7b04f5b08ed13006a8e8b60d792e1c90
Alex <dnjscksdn98@gmail.com>
2020-01-30 13:07:13 +0900

    Add: login, logout view for the browsable API

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
+from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
     path('', include('snippets.urls')),
+]
+
+# adding login to the browsable API
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
