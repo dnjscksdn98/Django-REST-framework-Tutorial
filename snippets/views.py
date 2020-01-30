9ff711d4e312befb1179c1d9994bc74c1af92534
Alex <dnjscksdn98@gmail.com>
2020-01-30 14:18:08 +0900

    Add endpoints for the root and the highlight

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,33 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, renderers
 from django.contrib.auth.models import User
-from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
+
+# creating an endpoint for the root of out API
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    # creating an endpoint for the highlighted snippets
+    # we don't want to use JSON, but instead just present an HTML representation
+    # there's no existing concrete generic view that we can use.
+    # we're not returning an object instance, but instead a property of an object instance
+    # so we'll use the base class for representing instances, and create our own get() method.
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -15,6 +39,7 @@ class SnippetList(generics.ListCreateAPIView):
     # associate snippets with users
     # the create() method of our serializer will now be passed an addtional 'owner' field,
     # along with the validated data from the request
+    # request.user : Django User instance
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
