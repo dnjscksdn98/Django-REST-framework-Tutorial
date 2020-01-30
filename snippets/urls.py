0083365febc986cdf3ba66b4844f611ba437d5fb
Alex <dnjscksdn98@gmail.com>
2020-01-30 14:18:35 +0900

    Wire up root and highlight URLs

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.api_root),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
-
