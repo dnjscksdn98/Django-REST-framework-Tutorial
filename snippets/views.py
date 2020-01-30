7cd19ca2ae69ffb318df811c4fef924ea73f9331
Alex <dnjscksdn98@gmail.com>
2020-01-30 13:21:13 +0900

    Add: custom permission to snippet-detail

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import permissions
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -22,7 +23,8 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     # Retrieve, update, or delete a code snippet
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 # create read-only views for the user representations
