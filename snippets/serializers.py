a47c8b473c86c338e2b2c94e38814b79cd8c28f4
Alex <dnjscksdn98@gmail.com>
2020-01-30 14:46:48 +0900

    Refactor serializer class - Hyperlinking API

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,27 +3,25 @@ from django.contrib.auth.models import User
 from .models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     # now that snippets are associated with the user that created them,
     # add the serializer field to reflect that
     # source : controls which attribute is used to populate a field, and can point at any attribute on the serialized instance
     # CharField(read_only=True)
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos',
-                  'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    # snippets is a reverse relationship on the User model,
-    # it will not be included by default when using the ModelSerializer class,
-    # so we needed to add an explicit field for it
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
-    )
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
