c93fd650ace1afcd160bb56312e08d2ee6ac0d8e
Alex <dnjscksdn98@gmail.com>
2020-01-31 17:23:02 +0900

    Update file

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -8,9 +8,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request
-        # so we'll always allow GET, HEAD, or OPTIONS requests.
+        # Check permissions for read-only request
+        # SAFE_METHODS : which is a tuple containing 'GET', 'OPTIONS' and 'HEAD'.
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the owner of the object.
+        # Check permissions for write request
         return obj.owner == request.user
